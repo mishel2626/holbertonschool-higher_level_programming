c40f420b0b09ef2894179ec226e9f7e3879da30c
mishel2626 <mishelspaho@gmail.com>
2026-03-04 17:27:21 +0100

    initial commit

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- script to list all databases
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,name VARCHAR(256));
