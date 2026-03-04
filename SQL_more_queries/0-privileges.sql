7bc4dc4fd42cfcaf583c87cc2cd0eae3ef9af90d
mishel2626 <mishelspaho@gmail.com>
2026-03-04 17:19:14 +0100

    initial commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,19 @@
--- script My privileges!
+-- script to list all databases
+
+-- Show all privileges for user user_0d_1 connected from localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show all privileges for user user_0d_2 connected from localhost
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
+-- -- write a script that print all privileges of user : user_0d_1
+-- -- create users 
+-- CREATE USER 'user_0d_1'@'localhost';
+-- CREATE USER 'user_0d_2'@'localhost';
+
+-- -- grant users privileges
+-- GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- -- write a script that print all privileges of user : user_0d_2
+-- SHOW GRANTS FOR 'user_0d_2'@'localhost';
